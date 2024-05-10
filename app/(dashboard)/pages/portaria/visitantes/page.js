ae1a37af31627af2485a8b1041b5e2817e5740cf
caio <zhj380@gmail.com>
2024-05-10 13:51:42 -0300

    caio

--- app/(dashboard)/pages/portaria/visitantes/page.js
+++ app/(dashboard)/pages/portaria/visitantes/page.js
@@ -1,4 +1,6 @@
+'use client'
 import * as React from "react";
+
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
@@ -7,6 +9,18 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+
+// modal 
+import Modal from '@mui/material/Modal';
+
+// components
+import NewVisitantes from "sub-components/portaria/visitantes/modal/newVisitantes";
+
+
+import InputBase from "@mui/material/InputBase";
+
 
 const bull = (
   <Box
@@ -17,64 +31,46 @@ const bull = (
   </Box>
 );
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+
 export default function Visitantes() {
+
+  const [openVisitantes, setOpenVisitantes] = React.useState(false);
+  const handleOpenVisitantes = () => setOpenVisitantes(true);
+  const handleCloseVisitantes = () => setOpenVisitantes(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-
-
         <Grid item xs={3.8} marginTop={2} marginLeft={2}>
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
               <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
+                variant="h5"
+                align="center"
+                component="h1"
+                color={"CaptionText"}
               >
-                Word of the Day
+                Total de visitantes
               </Typography>
-              <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
+              <Typography variant="h3" color="text.secondary" align="center">
+                50
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-        <Grid xs={3.8} marginTop={4} marginLeft={2}  >
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
+              <Button size="small">Saiba mais</Button>
             </CardActions>
           </Card>
         </Grid>
@@ -83,52 +79,86 @@ export default function Visitantes() {
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
               <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
+                variant="h5"
+                align="center"
+                component="h1"
+                color={"CaptionText"}
               >
-                Word of the Day
+                Total de Hóspedes
               </Typography>
-              <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
+              <Typography variant="h3" color="text.secondary" align="center">
+                12
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Learn More</Button>
+              <Button size="small">Saiba mais</Button>
             </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid xs={3.8} marginTop={4} marginLeft={2}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                variant="h5"
+                align="center"
+                component="h1"
+                color={"CaptionText"}
+              >
+                Prestadores de Serviço
+              </Typography>
+              <Typography variant="h3" color="text.secondary" align="center">
+                5
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Saiba mais</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        {/* area de pesquisa e botao  */}
+        <Grid xs={11.5} marginTop={2} marginLeft={4}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Stack direction="row" spacing={1} marginLeft={7}>
+                <div>
+                  <Paper
+                    component="form"
+                    sx={{
+                      p: "2px 4px",
+                      display: "flex",
+                      alignItems: "center",
+                      width: 800,
+                    }}
+                  >
+                    <InputBase
+                      sx={{ ml: 1, flex: 1 }}
+                      placeholder="Procurar Visitante"
+                      inputProps={{ "aria-label": "search google maps" }}
+                    />
+                  </Paper>
+                </div>
+                <div>
+                <Button variant="contained">Novo Visitante</Button>
+               
+                </div>
+
+                <div marginLeft="50">
+                <Button variant="contained">Novo Hóspede</Button>
+                </div>
+
+                <div marginLeft="50">
+                <Button variant="contained">Novo Prestador de Serviço</Button>
+                </div>
+              </Stack>
+            </CardContent>
+           
           </Card>
         </Grid>
 
         <Grid xs={11.5} marginTop={4} marginLeft={4}>
-        <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                area de pesquisa e botao 
-              </Typography>
-             
-            
-             
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-             <Grid xs={11.5} marginTop={4} marginLeft={4}>
-        <Card sx={{ minWidth: 275 }}>
+          <Card sx={{ minWidth: 275 }}>
             <CardContent>
               <Typography
                 sx={{ fontSize: 14 }}
@@ -137,17 +167,12 @@ export default function Visitantes() {
               >
                 area tabela
               </Typography>
-             
-            
-             
             </CardContent>
             <CardActions>
               <Button size="small">Learn More</Button>
             </CardActions>
           </Card>
-        </Grid>        
-       
-       
+        </Grid>
       </Grid>
     </Box>
   );
---
+++ sub-components/portaria/visitantes/modal/newVisitantes.js
@@ -0,0 +1,7 @@
+export default function NewVisitantes(){
+
+
+    return (
+        <>testes</>
+    )
+}
